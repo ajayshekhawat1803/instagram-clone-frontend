769d7aabf1616d930a90d049fe4ef97f59acebf1
ajayshekhawat1803 <ajayshekhawat1803@gmail.com>
2024-01-09 00:01:56 +0530

    Trying to use scheduler in vercel

--- src/api/backend-check.js
+++ src/api/backend-check.js
@@ -3,20 +3,20 @@
 import axios from 'axios';
 
 export default async function handler(req, res) {
-  try {
-    // Replace 'YOUR_BACKEND_URL' with the actual URL of your backend
-    const response = await axios.get('YOUR_BACKEND_URL');
-
-    // Check if the response indicates successful connectivity
-    if (response.status === 200) {
-      console.log('Backend is reachable!');
-      res.status(200).send('Backend is reachable!');
-    } else {
-      console.error('Unexpected status code:', response.status);
-      res.status(response.status).send('Unexpected status code: ' + response.status);
+    try {
+        // Replace 'YOUR_BACKEND_URL' with the actual URL of your backend
+        const response = await axios.get(process.env.REACT_APP_SERVER_URL);
+        console.log("Server Link-",process.env.REACT_APP_SERVER_URL);
+        // Check if the response indicates successful connectivity
+        if (response.status === 200) {
+            console.log('Backend is reachable!');
+            res.status(200).send('Backend is reachable!');
+        } else {
+            console.error('Unexpected status code:', response.status);
+            res.status(response.status).send('Unexpected status code: ' + response.status);
+        }
+    } catch (error) {
+        console.error('Error connecting to the backend:', error.message);
+        res.status(500).send('Error connecting to the backend: ' + error.message);
     }
-  } catch (error) {
-    console.error('Error connecting to the backend:', error.message);
-    res.status(500).send('Error connecting to the backend: ' + error.message);
-  }
 }
